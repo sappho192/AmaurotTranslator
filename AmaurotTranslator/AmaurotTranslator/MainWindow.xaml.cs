063d0c61d97121987dac7e72faf7819511900bcb
TAE-IN Sappho KIM <sappho192@gmail.com>
2021-03-21 13:39:29 +0900

    Add JP<->KO switch feature

--- AmaurotTranslator/AmaurotTranslator/MainWindow.xaml.cs
+++ AmaurotTranslator/AmaurotTranslator/MainWindow.xaml.cs
@@ -12,9 +12,17 @@ namespace AmaurotTranslator
     {
         bool isTranslatorBusy = false;
         ChromeOptions chromeOptions = new ChromeOptions();
+
+        string sk = "ko";
+        string tk = "ja";
+        const int STATE_K2J = 1;
+        const int STATE_J2K = 2;
+        int currentState = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            currentState = STATE_K2J;
             var driverService = ChromeDriverService.CreateDefaultService();
             driverService.HideCommandPromptWindow = true;
             chromeOptions.AddArguments("--headless");
@@ -34,8 +42,6 @@ namespace AmaurotTranslator
 
         private void Translate()
         {
-            string sk = "ko";
-            string tk = "ja";
             string sentence = tbOriginal.Text;
             string testUrl = $"https://papago.naver.com/?sk={sk}&tk={tk}&st={Uri.EscapeDataString(sentence)}";
             Context.Instance.browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
@@ -57,10 +63,8 @@ namespace AmaurotTranslator
         }
         private void ReTranslate()
         {
-            string sk = "ja";
-            string tk = "ko";
             string sentence = tbTranslated.Text;
-            string testUrl = $"https://papago.naver.com/?sk={sk}&tk={tk}&st={Uri.EscapeDataString(sentence)}";
+            string testUrl = $"https://papago.naver.com/?sk={tk}&tk={sk}&st={Uri.EscapeDataString(sentence)}";
             Context.Instance.browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
             Context.Instance.browser.Navigate().GoToUrl(testUrl);
             try
@@ -97,6 +101,40 @@ namespace AmaurotTranslator
         {
             Window window = (Window)sender;
             window.Topmost = true;
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btJKSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentState == STATE_K2J)
+            {
+                currentState = STATE_J2K;
+                btJKSwitch.Content = "한→일";
+                sk = "ja";
+                tk = "ko";
+                tbSk.Text = "번역하고 싶은 말을 적어보세요. (일본어)";
+                tbTk.Text = "번역된 문장이 적히는 곳입니다. (한국어)";
+                tbTk2Sk.Text = "번역된 한국어를 다시 일본어로 번역한 결과입니다.";
+                tbOriginal.Text = "";
+                tbTranslated.Text = "";
+                tbReTranslated.Text = "";
+            } else
+            {
+                currentState = STATE_K2J;
+                btJKSwitch.Content = "일→한";
+                sk = "ko";
+                tk = "ja";
+                tbSk.Text = "번역하고 싶은 말을 적어보세요. (한국어)";
+                tbTk.Text = "번역된 문장이 적히는 곳입니다. (일본어)";
+                tbTk2Sk.Text = "번역된 일본어를 다시 한국어로 번역한 결과입니다.";
+                tbOriginal.Text = "";
+                tbTranslated.Text = "";
+                tbReTranslated.Text = "";
+            }
         }
     }
 }
