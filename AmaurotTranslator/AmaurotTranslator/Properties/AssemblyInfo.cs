a9a101efaf52b40ab6a042147a3c4592e2fae552
Sappho <sappho192@gmail.com>
2022-08-15 02:46:41 +0900

    Update copyright

--- AmaurotTranslator/AmaurotTranslator/Properties/AssemblyInfo.cs
+++ AmaurotTranslator/AmaurotTranslator/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AmaurotTranslator")]
-[assembly: AssemblyCopyright("Copyright ©sappho192  2021")]
+[assembly: AssemblyCopyright("Copyright ©sappho192  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
